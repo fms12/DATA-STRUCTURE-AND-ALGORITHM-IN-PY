5256e18a97e89207460a36b79face7b6eaefa6eb
Mayank Singh <68012074+fms12@users.noreply.github.com>
2020-10-30 16:50:57 +0530

    Create Birthday CakeCandles.py

---
+++ Birthday CakeCandles.py
@@ -0,0 +1,6 @@
+def birthdayCakeCandles(candles):
+    x=max(candles) # here you store the max vlue between the candles list
+    return candles.count(x) # count he value which come more than times 
+  candles_count = int(input().strip())
+  candles = list(map(int, input().rstrip().split()))
+  result = birthdayCakeCandles(candles)
