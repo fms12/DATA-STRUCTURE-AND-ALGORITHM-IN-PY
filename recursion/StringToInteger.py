b38fbd803faa2a09bf9f44d1211278a9ebc4e4e8
Mayank Singh <68012074+fms12@users.noreply.github.com>
2021-08-03 22:42:25 +0530

    Create StringToInteger.py

---
+++ recursion/StringToInteger.py
@@ -0,0 +1,13 @@
+# Convert String to Intgers By recursion
+# eg:'123' to 123
+
+def desending(s):
+    if(len(s)==1):
+        return ord(s[0]) - 48
+    res = desending(s[1:])
+    ros = ord(s[0])- 48
+    j = ros*(10**(len(s)-1))+res
+    return j
+s = input()
+print(desending(s))
+
