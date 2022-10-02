0d3c713bee4e2cc143bf0a4792d323f024d9800c
Utkarsh Kashyap <69443104+mrgentlemanus@users.noreply.github.com>
2022-10-02 13:51:45 +0530

    Update fibonacci.py
    Signed-off-by: Utkarsh Kashyap <69443104+mrgentlemanus@users.noreply.github.com>

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,11 +1,13 @@
+# Python program to display the Fibonacci sequence
 def fib(n):
   if n<=1:
     return n
   else:
-    return fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2)  #recursion call
 
 count = int(input("Enter the limit: "))
 
+# check if the number of terms is valid
 if count<=0:
   print("Enter a number greater than 0")
 else:
