28eec4b0cdd3419ea325fc54801ee242273f849b
karanS08 <66710785+karanS08@users.noreply.github.com>
2022-10-02 11:50:06 +0530

    Create selection_sort.py
    Signed-off-by: karanS08 <66710785+karanS08@users.noreply.github.com>

---
+++ Searching & sorting/selection_sort.py
@@ -0,0 +1,15 @@
+# Selecion sort 
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+if __name__ == "__main__":
+    arr = [64, 25, 12, 22, 11]
+    print(selection_sort(arr))
+    
