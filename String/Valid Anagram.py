e5599a19ff2d97fda6c50c8e1ab0fbdaf08f3b73
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-10-02 02:13:00 -0500

    valid anagram

---
+++ String/Valid Anagram.py
@@ -0,0 +1,16 @@
+def isAnagram(s1, s2):
+  # If both strings don't use the same letters with the same frequencies,  
+  # then it's not an anagram, else it is.
+  counter = {}
+  
+  for char in s1:
+    counter[char] = counter.get(char, 0) + 1
+    
+  for char in s2:
+    counter[char] = counter.get(char, 0) - 1
+    
+  # If s2 is an anagram of s1, then all the values of our counter should be 0.
+  return all(val == 0 for val in counter.values())
+
+print(isAnagram('anagram', 'nagaram')) # True
+print(isAnagram('rat', 'car')) # False
