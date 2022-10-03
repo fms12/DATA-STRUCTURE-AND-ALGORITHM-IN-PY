ecb30201d7ab9da6cc255c1716307a8982d6db1a
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2022-10-03 19:56:30 +0530

    Update Display_array.py
    Added comments to Display_array.py

--- recursion/Basic Array Questions/Display_array.py
+++ recursion/Basic Array Questions/Display_array.py
@@ -3,15 +3,27 @@
 def display_arr(a,i,n):
     if i>=n: # Base Case if value of i become greater or equal to size of an array then it returns
         return
-    print(a[i],end=" ")
-    display_arr(a,i+1,n)
+    print(a[i],end=" ") # Print the array at ith position (as the value of i increases elements get print accordingly)
+    display_arr(a,i+1,n) # Function calling itself
 
-arr = []
-n = int(input("Enter the size of the array: "))
+arr = [] # Taking an Array
+n = int(input("Enter the size of the array: ")) # Initializing the size of an array
 print("Enter the elements of the array: ")
 for i in range(0,n):
-    num = int(input())
+    num = int(input()) # Taking input from user 
     arr.append(num)
 
-print("Elements of Array are: ")
-display_arr(arr,0,n)
+print("Elements of Array are: ") 
+display_arr(arr,0,n) # Printing the elements of an array through recursion
+
+# Example
+
+# Input: 
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# Ouput:
+1 2 3 4 5
