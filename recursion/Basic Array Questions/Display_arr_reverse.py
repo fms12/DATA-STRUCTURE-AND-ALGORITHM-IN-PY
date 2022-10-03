a7b2b6ef194e0487e6ab391675feafb59f40712b
Ratnesh Kumar <89133941+ratneshrt@users.noreply.github.com>
2022-10-03 19:50:54 +0530

    Update Display_arr_reverse.py
    Comments added to display_arr_reverse.py

--- recursion/Basic Array Questions/Display_arr_reverse.py
+++ recursion/Basic Array Questions/Display_arr_reverse.py
@@ -4,9 +4,12 @@ def reverse(a,n):
     if n==0: # Base Case when size of array is 0 then it returns
         return
     else:
-        print(a[n-1])
-        reverse(a,n-1)
+        print(a[n-1],end=" ") # Print the last element of the array
+        reverse(a,n-1) # Function calling itself 
 
-a = [1, 42, 45, 60, 50, 10, 2]
+a = [1, 42, 45, 60, 50, 10, 2] # Taking an array
 n = len(a)
 reverse(a,n)
+
+# Output
+# 2 10 50 60 45 42 1
