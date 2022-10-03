0bbe03dc7d0e1e312e84e8fd17ac03e5fc08bc00
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-10-03 15:30:15 +0530

    Create Count_0.py

---
+++ recursion/MathsQ/Count_0.py
@@ -0,0 +1,7 @@
+number=int(input("Enter the number :"))
+to_String=str(number)
+count=0
+for i,element in enumerate(to_String):
+    if to_String[i] is "0":
+        count=count+1
+print(count)     
