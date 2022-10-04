88cbfa2af0ce509afd2a3f4a2f0bf78b2255ad81
Mouri Roy <87095935+lognoroy@users.noreply.github.com>
2022-10-04 11:14:45 +0530

    Create SelectionSort.py

---
+++ Searching & sorting/Sorting/SelectionSort.py
@@ -0,0 +1,29 @@
+# Selection sort in Python
+# Time Complexity	 
+# Best	O(n2)
+# Worst	O(n2)
+# Average	O(n2)
+# Space Complexity	O(1)
+# Stability	No
+
+def selectionSort(array, size):
+   
+    for step in range(size):
+        min_idx = step
+
+        for i in range(step + 1, size):
+         
+            # to sort in descending order, change > to < in this line
+            # select the minimum element in each loop
+            if array[i] < array[min_idx]:
+                min_idx = i
+         
+        # put min at the correct position
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+
+data = [-2, 45, 0, 11, -9]
+size = len(data)
+selectionSort(data, size)
+print('Sorted Array in Ascending Order:')
+print(data)
