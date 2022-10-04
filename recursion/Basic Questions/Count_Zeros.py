60873ff5ea165995488a4572ccf89baa76457957
misbahulhaque234 <misbahulhaque234@gmail.com>
2022-10-04 16:14:32 +0530

    recursion maths questions

---
+++ recursion/Basic Questions/Count_Zeros.py
@@ -0,0 +1,13 @@
+#Program for finding the number of  zeros in a given number
+def countZeros(n):
+    if n == 0:  # base case
+        return 0  # return 0 if n == 0
+    smallOutput = countZeros(n // 10)  # recursion call
+    # adding and smallOutput vaule + 1 and divide using 2^n
+    if n % 10 == 0:
+        return smallOutput + 1
+    else:
+        return smallOutput
+
+n = int(input("Enter the number: "))  # taking input from user
+print(countZeros(n)) #calling the function and printing the result
