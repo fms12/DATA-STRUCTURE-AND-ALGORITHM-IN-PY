c313fdc1508c2b11286b56b6eaba8a3bb6fc5df2
Kevin Cao <kevincao@kevincao.xyz>
2022-10-08 09:45:00 -0400

    Add script to replace PI with 3.14

--- String/replace_all_pi.py
+++ String/replace_all_pi.py
@@ -1,2 +1,11 @@
+def replace(s: str, old: str, new: str) -> str:
+    if not s:
+        return ''
+
+    if s.startswith(old):
+        s = new + s[len(old):]
+
+    return s[0] + replace(s[1:], old, new)
+
 for i in range(int(input())):
-    print(input().replace('pi', '3.14'))
+    print(replace(input(), 'pi', '3.14'))
