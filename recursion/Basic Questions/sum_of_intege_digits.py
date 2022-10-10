b24f1fe22310296c09026fdf5e369d8d61f22e75
Vinayreddy18 <86468467+Vinayreddy18@users.noreply.github.com>
2022-10-10 19:13:03 +0530

    Update sum_of_intege_digits.py

--- recursion/Basic Questions/sum_of_intege_digits.py
+++ recursion/Basic Questions/sum_of_intege_digits.py
@@ -1,14 +1,14 @@
 # Python program to
-# compute sum of digits in
+# compute sum of digits in 
 # number.
-
-# Function to get sum of digits
+   
+# Function to get sum of digits 
 def getSum(n):
-	
-	sum = 0
-	for digit in str(n):
-	sum += int(digit)	
-	return sum
-
+    
+    sum = 0
+    for digit in str(n): 
+      sum += int(digit)      
+    return sum
+   
 n = 12345
 print(getSum(n))
