3c963c65aa475c610e72a3a4f2da9972952bd4d6
pranav <94703612+dammalapat@users.noreply.github.com>
2022-10-10 19:40:02 +0530

    duplicate.py

---
+++ function and array/duplicate.py
@@ -0,0 +1,17 @@
+#  find Duplicate in the array
+# left most repeating element 
+#  its can also work for the strings
+
+for i in range(len(arr)):
+    count[arr[i]]+=1
+for j in range(len(arr)):
+    if(count[arr[j]]>1):
+        return arr[j]
+return -1
+# 2 approch
+for i in range(len(arr)):
+    if(count[arr[i]] is False):
+        count[arr[i]] = True
+    else:
+        return arr[i]
+return -1
