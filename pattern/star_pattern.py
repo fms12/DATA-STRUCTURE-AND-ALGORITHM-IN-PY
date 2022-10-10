b59bd04a203c580d1bf2b798af6d68e78e11141c
Vinayreddy18 <86468467+Vinayreddy18@users.noreply.github.com>
2022-10-10 19:49:25 +0530

    star_pattern.py

---
+++ pattern/star_pattern.py
@@ -0,0 +1,9 @@
+n = int(input("Enter value: "))
+for i in range(n):
+    #Two internal loops - first will create spaces
+    for j in range(1, n - i):
+        print(" ", end="")
+        #second will print number
+    for k in range(1, i + 2):
+        print(k, end='')
+    print()
