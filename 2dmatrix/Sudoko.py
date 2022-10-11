22b811d3c3f8481240041fe804d66100bc332521
theexplorer172 <115581847+theexplorer172@users.noreply.github.com>
2022-10-11 23:41:11 +0530

    Update Sudoko.py

--- 2dmatrix/Sudoko.py
+++ 2dmatrix/Sudoko.py
@@ -1,99 +1,46 @@
 class Solution:
-    col_size = 9  # len(self.board)
-    row_size = 9  # len(self.board[0])
-    block_col_size = 3
-    block_row_size = 3
-    digits = '123456789'
-    empty_symbol = '.'
-
-    # def solveSudoku(self, board: List[List[str]]) -> None:
-    def solveSudoku(self, board):
-        self.init(board)
-        self.solve()
-
-    def init(self, board):
-        self.board = board
-
-        # list all empty cells. a `cell` is a tuple `(row_index, col_index)`
-        self.empty_cells = set([(ri, ci) for ri in range(self.row_size) for ci in range(self.col_size) if self.board[ri][ci] == self.empty_symbol])
-
-        # find candidates of each cell
-        self.candidates = [[set(self.digits) for ci in range(self.col_size)] for ri in range(self.row_size)]
-        for ri in range(self.row_size):
-            for ci in range(self.col_size):
-                digit = self.board[ri][ci]
-                if digit != self.empty_symbol:
-                    self.candidates[ri][ci] = set()
-                    self.update_candidates((ri, ci), digit)
-
-    def solve(self):
-        # if there are no empty cells, it's solved
-        if not self.empty_cells:
-            return True
-
-        # get the cell with fewest candidates
-        cell = min(self.empty_cells, key=lambda cell: len(self.candidates[cell[0]][cell[1]]))
-
-        # try filling the cell with one of the candidates, and solve recursively
-        ri, ci = cell
-        for digit in list(self.candidates[ri][ci]):
-            candidate_updated_cells = self.fill_cell(cell, digit)
-            solved = self.solve()
-            if solved:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row = collections.defaultdict(set) # r: set("1", "2", ...)
+        col = collections.defaultdict(set) # c: set("1", "2", ...)      
+        block = collections.defaultdict(set) # temp: set("1", "2", ...)
+        unfilled = [] # unfilled cells
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    row[r].add(board[r][c]) # add num filled in each row
+                    col[c].add(board[r][c]) # add num filled in each column
+                    block[(r // 3, c // 3)].add(board[r][c]) # add num filled in each 3*3 block
+                else:
+                    unfilled.append((r, c)) # add unfilled cells
+        
+        def dfs():
+            if len(unfilled) == 0: # finished if no more unfilled cells
                 return True
-            self.unfill_cell(cell, digit, candidate_updated_cells)
+            r, c = unfilled[-1] # get the last pair of (r, c) from unfilled
+            temp = (r // 3, c // 3) # convert index 0 ~ 2 to 0, 3 ~ 5 to 1, 6 ~ 8 to 2
+            for num in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
+                if num in row[r] or num in col[c] or num in block[temp]: # skip if num is already filled
+                    continue
+                
+                # below are normal steps for backtracking
+                board[r][c] = num
+                row[r].add(num)
+                col[c].add(num)
+                block[temp].add(num)
+                unfilled.pop()
+                
+                if dfs():
+                    return True
 
-        # if no solution found, go back and try the next candidates
-        return False
-
-    def fill_cell(self, cell, digit):
-        # fill the cell with the digit
-        ri, ci = cell
-        self.board[ri][ci] = digit
-
-        # remove the cell from empty_cells
-        self.empty_cells.remove(cell)
-
-        # update the candidates of other cells
-        # keep a list of updated cells. will be used when unfilling cells
-        candidate_updated_cells = self.update_candidates(cell, digit)
-
-        return candidate_updated_cells
-
-    def unfill_cell(self, cell, digit, candidate_updated_cells):
-        # unfill cell
-        ri, ci = cell
-        self.board[ri][ci] = self.empty_symbol
-
-        # add the cell back to empty_cells
-        self.empty_cells.add(cell)
-
-        # add back candidates of other cells
-        for ri, ci in candidate_updated_cells:
-            self.candidates[ri][ci].add(digit)
-
-    def update_candidates(self, filled_cell, digit):
-        candidate_updated_cells = []
-        for ri, ci in self.related_cells(filled_cell):
-            if (self.board[ri][ci] == self.empty_symbol) and (digit in self.candidates[ri][ci]):
-                self.candidates[ri][ci].remove(digit)
-                candidate_updated_cells.append((ri, ci))
-        return candidate_updated_cells
-
-    def related_cells(self, cell):
-        return list(set(self.cells_in_same_row(cell) + self.cells_in_same_col(cell) + self.cells_in_same_block(cell)))
-
-    def cells_in_same_row(self, cell):
-        return [(cell[0], ci) for ci in range(self.col_size)]
-
-    def cells_in_same_col(self, cell):
-        return [(ri, cell[1]) for ri in range(self.row_size)]
-
-    def cells_in_same_block(self, cell):
-        block_first_cell_ri = (cell[0] // self.block_row_size) * self.block_row_size
-        block_first_cell_ci = (cell[1] // self.block_col_size) * self.block_col_size
-        return [
-            (block_first_cell_ri + in_block_ri, block_first_cell_ci + in_block_ci)
-            for in_block_ri in range(self.block_row_size)
-            for in_block_ci in range(self.block_col_size)
-        ]
+                board[r][c] = "."
+                row[r].remove(num)
+                col[c].remove(num)
+                block[temp].remove(num)
+                unfilled.append((r, c))
+            return False
+        
+        dfs()
