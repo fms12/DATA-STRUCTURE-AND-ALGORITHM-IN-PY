8e951e529101095969bf7ff3f407d8ea960316b3
theexplorer172 <115581847+theexplorer172@users.noreply.github.com>
2022-10-11 23:42:41 +0530

    Update Sudoko.py

--- 2dmatrix/Sudoko.py
+++ 2dmatrix/Sudoko.py
@@ -1,46 +1,114 @@
-class Solution:
-    def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        row = collections.defaultdict(set) # r: set("1", "2", ...)
-        col = collections.defaultdict(set) # c: set("1", "2", ...)      
-        block = collections.defaultdict(set) # temp: set("1", "2", ...)
-        unfilled = [] # unfilled cells
-        
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] != ".":
-                    row[r].add(board[r][c]) # add num filled in each row
-                    col[c].add(board[r][c]) # add num filled in each column
-                    block[(r // 3, c // 3)].add(board[r][c]) # add num filled in each 3*3 block
-                else:
-                    unfilled.append((r, c)) # add unfilled cells
-        
-        def dfs():
-            if len(unfilled) == 0: # finished if no more unfilled cells
-                return True
-            r, c = unfilled[-1] # get the last pair of (r, c) from unfilled
-            temp = (r // 3, c // 3) # convert index 0 ~ 2 to 0, 3 ~ 5 to 1, 6 ~ 8 to 2
-            for num in {"1", "2", "3", "4", "5", "6", "7", "8", "9"}:
-                if num in row[r] or num in col[c] or num in block[temp]: # skip if num is already filled
-                    continue
-                
-                # below are normal steps for backtracking
-                board[r][c] = num
-                row[r].add(num)
-                col[c].add(num)
-                block[temp].add(num)
-                unfilled.pop()
-                
-                if dfs():
-                    return True
+# N is the size of the 2D matrix N*N
+N = 9
 
-                board[r][c] = "."
-                row[r].remove(num)
-                col[c].remove(num)
-                block[temp].remove(num)
-                unfilled.append((r, c))
-            return False
-        
-        dfs()
+# A utility function to print grid
+def printing(arr):
+	for i in range(N):
+		for j in range(N):
+			print(arr[i][j], end = " ")
+		print()
+
+# Checks whether it will be
+# legal to assign num to the
+# given row, col
+def isSafe(grid, row, col, num):
+
+	# Check if we find the same num
+	# in the similar row , we
+	# return false
+	for x in range(9):
+		if grid[row][x] == num:
+			return False
+
+	# Check if we find the same num in
+	# the similar column , we
+	# return false
+	for x in range(9):
+		if grid[x][col] == num:
+			return False
+
+	# Check if we find the same num in
+	# the particular 3*3 matrix,
+	# we return false
+	startRow = row - row % 3
+	startCol = col - col % 3
+	for i in range(3):
+		for j in range(3):
+			if grid[i + startRow][j + startCol] == num:
+				return False
+	return True
+
+# Takes a partially filled-in grid and attempts
+# to assign values to all unassigned locations in
+# such a way to meet the requirements for
+# Sudoku solution (non-duplication across rows,
+# columns, and boxes) */
+def solveSudoku(grid, row, col):
+
+	# Check if we have reached the 8th
+	# row and 9th column (0
+	# indexed matrix) , we are
+	# returning true to avoid
+	# further backtracking
+	if (row == N - 1 and col == N):
+		return True
+	
+	# Check if column value becomes 9 ,
+	# we move to next row and
+	# column start from 0
+	if col == N:
+		row += 1
+		col = 0
+
+	# Check if the current position of
+	# the grid already contains
+	# value >0, we iterate for next column
+	if grid[row][col] > 0:
+		return solveSudoku(grid, row, col + 1)
+	for num in range(1, N + 1, 1):
+	
+		# Check if it is safe to place
+		# the num (1-9) in the
+		# given row ,col ->we
+		# move to next column
+		if isSafe(grid, row, col, num):
+		
+			# Assigning the num in
+			# the current (row,col)
+			# position of the grid
+			# and assuming our assigned
+			# num in the position
+			# is correct
+			grid[row][col] = num
+
+			# Checking for next possibility with next
+			# column
+			if solveSudoku(grid, row, col + 1):
+				return True
+
+		# Removing the assigned num ,
+		# since our assumption
+		# was wrong , and we go for
+		# next assumption with
+		# diff num value
+		grid[row][col] = 0
+	return False
+
+# Driver Code
+
+# 0 means unassigned cells
+grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+		[5, 2, 0, 0, 0, 0, 0, 0, 0],
+		[0, 8, 7, 0, 0, 0, 0, 3, 1],
+		[0, 0, 3, 0, 1, 0, 0, 8, 0],
+		[9, 0, 0, 8, 6, 3, 0, 0, 5],
+		[0, 5, 0, 0, 9, 0, 6, 0, 0],
+		[1, 3, 0, 0, 0, 0, 2, 5, 0],
+		[0, 0, 0, 0, 0, 0, 0, 7, 4],
+		[0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+if (solveSudoku(grid, 0, 0)):
+	printing(grid)
+else:
+	print("no solution exists ")
+
