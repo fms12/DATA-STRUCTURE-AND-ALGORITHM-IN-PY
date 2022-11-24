f91ec50f1b8cfe0e9d09f81406e2ebddd8a8ee8d
Kd-Here <gamelover9899@gmail.com>
2022-11-25 00:26:02 +0530

    Provided testcases supporting pytest

--- Searching & sorting/Searching/BinarySearchTree/test_solution.py
+++ Searching & sorting/Searching/BinarySearchTree/test_solution.py
@@ -1,12 +1,32 @@
 from .solution import Build_Tree
-
+import pytest
 
 numbers = [1, 4, 10, 20, 9, 23, 18, 34]
-numbers_tree = Build_Tree(numbers)
-print("Input numbers:", numbers)
-print("Min:", numbers_tree.Find_Minimum_Node())
-print("Max:", numbers_tree.Find_Maximum_Node())
-print("Sum:", numbers_tree.calculate_Sum_Of_Nodes())
-print("In order traversal:", numbers_tree.In_Order_Traversal())
-print("Pre order traversal:", numbers_tree.Pre_Order_Traversal())
-print("Post order traversal:", numbers_tree.Post_Order_Traversal())
+Ans = sorted(numbers)
+
+def test_given_data():
+    assert numbers == numbers
+
+def test_sorted_node():
+    numbers_tree = Build_Tree(numbers)
+    assert numbers_tree.In_Order_Traversal() == Ans
+
+def test_min_ofnode():
+    numbers_tree = Build_Tree(numbers)
+    assert numbers_tree.Find_Minimum_Node() == Ans[0]
+
+def test_max_ofnode():
+    numbers_tree = Build_Tree(numbers)
+    assert numbers_tree.Find_Maximum_Node() == Ans[-1]
+
+def test_sum_ofnode():
+    numbers_tree = Build_Tree(numbers)
+    assert numbers_tree.calculate_Sum_Of_Nodes() == sum(Ans)
+ 
+def test_post_order():
+    numbers_tree = Build_Tree(numbers)
+    assert numbers_tree.Post_Order_Traversal() == [9, 18, 34, 23, 20, 10, 4, 1]
+
+def test_pre_order():
+    numbers_tree = Build_Tree(numbers)
+    assert numbers_tree.Pre_Order_Traversal() == [1, 4, 10, 9, 20, 18, 23, 34]
