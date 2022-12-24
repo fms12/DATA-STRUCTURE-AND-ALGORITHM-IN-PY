d311c2db642e2a9497ece99256be1e39bfd91cd6
Kd-Here <gamelover9899@gmail.com>
2022-12-24 18:26:23 +0530

    Update4

--- LinkedList/Circular/test_solution.py
+++ LinkedList/Circular/test_solution.py
@@ -11,13 +11,3 @@ def inserting_node():
     last3 = adding_elements.addFront(2)
     last4 = adding_elements.addAfter(10, 2)
     assert adding_elements.traverse() == print(last3,last4,last1,last2)
-
-
-def deleting_node():
-    delete = CircularLinkedList()
-    last = delete.addToEmpty(6)
-    last = delete.addEnd(8)
-    last = delete.addFront(2)
-    last = delete.addAfter(10, 2)
-    last = delete.deleteNode(last, 8)
-    assert delete.traverse() == print(2,10,6)
