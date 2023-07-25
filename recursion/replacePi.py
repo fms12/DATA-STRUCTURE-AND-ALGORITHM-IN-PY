aea2105c444d89b639097792caa144bddead20e8
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-07-25 14:06:48 +0300

    recursion
    implemented both into recursion

--- recursion/replacePi.py
+++ recursion/replacePi.py
@@ -1,8 +1,21 @@
 def desending(s):
-    s = s.replace("π", "3.14")
-    s = s.replace("pi", "3.14")
-    return s
+    if(len(s) == 1 and s != 'π'):
+        return s
+    elif(len(s) == 1 and s == 'π'):
+        return '3.14'
+
+    res = desending(s[1:])
+    if(s[0]=='p' and s[1]=='i'):
+        return '3.14'+res[1:]
+    elif (s[0] == 'π'):
+        return '3.14' + res
+    else:
+        return s[0]+res
+
+if __name__ == "__main__":
+    s = input()
+    print(desending(s))
 # INPUT
-# xpipippixx
+# xpipippixx, xπpπx
 # OUTPUT
 # x3.143.14p3.14xx
